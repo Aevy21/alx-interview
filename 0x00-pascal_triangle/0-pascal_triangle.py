4ce22b1c5a22d96f600ff42d364458599b130078
Avumile <Smithavery435@gmail.com>
2024-05-29 17:15:25 -0700

    readme , pascal triangle file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+This script defines a function to generate Pascal's triangle up to the nth row.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+
+    Args:
+        n (int): Number of rows for Pascal's triangle.
+
+    Returns:
+        list of lists: Pascal's triangle up to the nth row.
+
+    Raises:
+        ValueError: If n is less than or equal to 0.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
