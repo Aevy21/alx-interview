2b75cff650141f82900d2e57230df64d800ca8ec
Avumile <Smithavery435@gmail.com>
2024-06-11 15:34:36 -0700

    understanding the problem and breaking it down to smaller problems

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 
 """
-function to calculate the fewest number of operations needed to result in exact characters in a file using copy all and paste only
+func to calc the fewest number of operations needed to result in \
+        n  chars  in a file using copy all and paste.
 """
 
 
 def minOperations(n):
     """
-    Calculate the minimum number of copy and paste operations required to generate
+    Calculate the min number of copy and paste operations required to generate
     exactly n 'h' characters using the fewest operations.
-    
+
     Parameters:
     n (int): The target number of 'h' characters.
-    
+
     Returns:
     int: The minimum number of operations required.
     """
     if n == 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
