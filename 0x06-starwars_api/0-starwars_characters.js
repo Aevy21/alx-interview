36df00e4e13bd0e6f0e26173285d4744fa10d9b8
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-13 00:51:59 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,55 +1,32 @@
 #!/usr/bin/node
-// This script prints all characters of a Star Wars movie using the Star Wars API.
 
 const request = require('request');
-const process = require('process');
-
-if (process.argv.length < 3) {
-  console.error('Usage: print_characters.js <movie_id>');
-  process.exit(1);
-}
 
 const movieId = process.argv[2];
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const getMovieDetails = (movieId, callback) => {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-  request(url, (error, response, body) => {
+function requestCharacter (characterUrlList, idx) {
+  if (idx === characterUrlList.length) {
+    return;
+  }
+
+  request(characterUrlList[idx], (error, response, body) => {
     if (error) {
-      console.error('Error fetching movie details:', error);
-      process.exit(1);
+      console.error(error);
+    } else {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+
+      requestCharacter(characterUrlList, idx + 1);
     }
-    callback(JSON.parse(body));
   });
-};
+}
 
-const getCharacterName = (characterUrl, callback) => {
-  request(characterUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error fetching character name:', error);
-      callback(null);
-      return;
-    }
-    callback(JSON.parse(body).name);
-  });
-};
-
-const main = (movieId) => {
-  getMovieDetails(movieId, (movieDetails) => {
-    const characters = movieDetails.characters || [];
-    let count = 0;
-
-    characters.forEach((characterUrl) => {
-      getCharacterName(characterUrl, (name) => {
-        if (name) {
-          console.log(name);
-        }
-        count++;
-        if (count === characters.length) {
-          process.exit(0);
-        }
-      });
-    });
-  });
-};
-
-main(movieId);
+request(movieUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const charactersUrls = JSON.parse(body).characters;
+    requestCharacter(charactersUrls, 0);
+  }
+});
