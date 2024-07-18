d08609b63faede4ed861f80dd08f3eeb5ce38841
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-18 16:21:46 +0200

    Create 0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Rotate an n x n 2D matrix 90 degrees clockwise in-place.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    - matrix (list of list of int): The matrix to rotate. Must be square.
+    Returns:
+    - None: The matrix is edited in-place.
+    Raises:
+    - ValueError: If the input matrix is not square.
+    """
+
+    n = len(matrix)
+
+    if n != len(matrix[0]):
+        raise ValueError("Input matrix must be square")
+
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse();
